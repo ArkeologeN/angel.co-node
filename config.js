3e2e08075671afd0f94b0c1df4ce87896e75fb31
ArkeologeN <hamzawaqas.programmer@gmail.com>
2013-12-17 09:11:27 +0500

    Initial structure added w/ service hierarchy and inheritence

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by arkeologen on 12/17/13.
+ */
+
+module.exports = function() {
+    return {
+        auth_url: 'https://angel.co/api/oauth/authorize',
+        token_url: "https://angel.co/api/oauth/token"
+    }
+}();
